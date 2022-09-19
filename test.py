cd88e68ae2833e9707f6c5877c71369fde62aac6
Arnav Vikal <arnav.vikal7@gmail.com>
2022-09-19 19:16:55 +0530

    hello

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ print("helloworld")
 z=x+y
 
 for i in range(10):
-    print(z)
-
-git config --global user.email "arnav.vikal7@gmail.com"
+    print(z):
+git config --global user.email "arnav.vikal7@gmail.com":
 git config --global user.name "Arnav Vikal"
