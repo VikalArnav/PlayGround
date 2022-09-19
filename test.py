95365feb4184cf36f1b2734d94f250a60115c494
Arnav Vikal <arnav.vikal7@gmail.com>
2022-09-19 18:17:04 +0530

    git commit -a -m "git config --global user.email "arnav.vikal7@gmail.com"   git config --global user.name "Arnav Vikal"

---
+++ test.py
@@ -0,0 +1,12 @@
+x=10
+y=5
+
+print("helloworld")
+z=x+y
+
+for i in range(10):
+    print(z)
+
+
+ git config --global user.email "arnav.vikal7@gmail.com"
+  git config --global user.name "Arnav Vikal"
